1be0db3c5734a61d568123d57b3cab40a2060e03
TeRRa2386 <juancsilva86@gmail.com>
2025-05-27 20:49:56 -0400

    comment app.run

--- main.py
+++ main.py
@@ -57,5 +57,5 @@ def contact():
 
     return render_template('contact.html', form=form)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
